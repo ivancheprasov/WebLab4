60c84b2b984f0cd97fa9f50adc689c841f629280
ivancheprasov <kabasica291@gmail.com>
2020-01-09 17:07:38 +0300

    Added link to git

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,7 @@ export function Footer(props) {
     <div className="footer" style={props.style}>
         <p>Ссылка на исходные файлы:
             <a id="gitLink" href="https://github.com/ivancheprasov/WebLab4">github.com/ivancheprasov/WebLab4</a>
+            <a id="gitLink" href="https://github.com/NotNaturalSelection/back4lab4">github.com/NotNaturalSelection/back4lab4</a>
         </p>
     </div>
     );
